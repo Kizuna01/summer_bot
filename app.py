d93352a001facd2c182c978c7c656bae680d63f1
sushi <79702494+Kizuna01@users.noreply.github.com>
2023-10-25 10:48:46 +0800

    Update app.py

--- app.py
+++ app.py
@@ -76,146 +76,37 @@ def handle_message(event):
     
     global status
     
-    if '自行車' in msg:
-        msg=getbike()
-        message = TextSendMessage(text=msg)
-    elif '空氣' in msg:
-        status=2
-        msg = '請輸入地區'
-        message = TextSendMessage(text=msg)
-    elif '便當' in msg :
-        foodout=[]
-        food=['https://dogbaby2266.com/wp-content/uploads/2021/08/Miss-Energy-%E8%83%BD%E9%87%8F%E5%B0%8F%E5%A7%90-%E7%9B%B4%E7%87%9F%E8%8F%9C%E5%96%AE-min.jpg',#能量小姐
-             'https://cdn.ftvnews.com.tw/summernotefiles/News/fd7369ff-a327-4405-8f73-1b27587453e4.jpg']#池上木盒
-        for i in food:
-            foodout.append(ImageSendMessage(original_content_url=i,preview_image_url=i))
-        line_bot_api.reply_message(event.reply_token,foodout)
-    elif '冰' in msg:
-        message=LocationSendMessage(title='冰',address='台中市火車站前',latitude=24.1379641,longitude=120.6834481)
-    elif '選擇' in msg:
+    if '指令' == msg:
+        message = TextSendMessage(text="/選擇 A B C \n 抽 \n 你家爆炸 \n 大樂透")
+   
+    #elif '冰' in msg:
+    #    message=LocationSendMessage(title='冰',address='台中市火車站前',latitude=24.1379641,longitude=120.6834481)
+    elif '/選擇' in msg:
         choice=msg.split(' ')
         x=random.randint(1,len(choice))
         msg='就決定是{}'.format (choice[x])
         message = TextSendMessage(text=msg)
-    elif '熱量小幫手' in msg:
-        msg='火鍋 早餐 便當 水果 \n subway 壽司 夜市 中式'
-        message = TextSendMessage(text=msg)
-    elif '/抽' == msg:
+    elif '抽' == msg:
             pretty=['https://ms0728707.000webhostapp.com/summer_img/emaster.jpg',
                     'https://ms0728707.000webhostapp.com/summer_img/one.jpg',
+                    'https://ms0728707.000webhostapp.com/summer_img/dead01.jpg',
                     ]
             x=random.randint(0,2)
             imgurl=pretty[x]
             message = ImageSendMessage(original_content_url=imgurl,preview_image_url=imgurl)
-    elif '復仇者' in msg:
-            avengers=['https://assets.juksy.com/files/articles/48057/800x_100_w-567a8ea3be9d0.jpg','https://img.ltn.com.tw/Upload/ent/page/800/2017/07/11/phpBls3Lw.jpg',
-                    'https://images.chinatimes.com/newsphoto/2020-05-22/656/20200522001767.jpg','https://img.ltn.com.tw/Upload/ent/page/800/2017/07/31/phpaOdu0d.png',
-                    'https://static.ettoday.net/images/3500/d3500323.jpg','https://assets.juksy.com/files/articles/89548/800x_100_w-5cb3ead749441.jpg',
-                    'https://img.cinemablend.com/quill/0/0/5/e/4/1/005e41d016dfd922285dbd3bec4937aa416949ba.jpg','https://www.teepr.com/wp-content/uploads/2020/07/20190526132405_85.jpg',
-                    'https://images.chinatimes.com/newsphoto/2020-07-15/656/20200715004880.jpg','https://img.ttshow.tw/images/media/frontcover/2020/08/20/004_hxtbiob.jpg',
-                    'https://attach.setn.com/newsimages/2021/03/27/3085088-PH.jpg','https://news.agentm.tw/wp-content/uploads/black-widow-movie_1515653087555-768x480-1.jpg',
-                    'https://inmywordz.com/wp-content/uploads/20180504163013_39.jpg','https://hk.ulifestyle.com.hk/cms/images/topic/1024x576/201907/20190731145218_0_maxresdefault.jpg',
-                    'https://media.gq.com.tw/photos/604733815c98c68ff6ed0acc/master/pass/scarlet-witch-wandavision.png','https://www.teepr.com/wp-content/uploads/2019/12/https___hk.hypebeast.com_files_2018_05_elizabeth-olsen-want-avengers-costume-cover-cleavage-00-1.jpg',
-                    'https://news.agentm.tw/wp-content/uploads/thanos-snap-on-earth.jpg','https://i.beauty321.com/816x/https://il.beauty321.com/gallery/albums_photo/10963-201905101552462906.jpg',
-                    'https://www.teepr.com/wp-content/uploads/2019/05/%E7%BE%8E%E9%9A%8A%E7%9B%BE%E7%89%8CA.jpg','https://news.agentm.tw/wp-content/uploads/Thanos-hot-toy-cover.png',
-                    'https://cdn2.ettoday.net/images/4043/d4043686.jpg','https://news.agentm.tw/wp-content/uploads/000-7.png',
-                    'https://media.altpress.com/uploads/2020/09/Captain-America-Avengers-Endgame-min.jpg','https://cdn2.ettoday.net/images/3342/d3342642.jpg',
-                    'https://news.agentm.tw/wp-content/uploads/4711921-untitled-1.jpg','https://news.agentm.tw/wp-content/uploads/67584235_2277918025597263_91842927822635008_n-%E6%8B%B7%E8%B2%9D-750x422.jpg',
-                    'https://news.agentm.tw/wp-content/uploads/2017/10/Valkyrie-Ragnarok-Thor-1-1024x576.png','http://i1.hdslb.com/bfs/archive/e27158543580bc761b949cc25dd7df8937420ea5.jpg',
-                    ]
-            y=random.randint(0,29)
-            imgurl=avengers[y]
-            message = ImageSendMessage(original_content_url=imgurl,preview_image_url=imgurl)
-    elif '水果' in msg:
-        imgurl='https://www.healingdaily.com.tw/media/images/2020/11/124820476_3673846755999006_3499646163171754703_o_1605157386.jpg'#水果
+  
+    elif '你家爆炸' == msg:
+        imgurl='https://ms0728707.000webhostapp.com/summer_img/bomb.jpg'#你家爆炸
         message = ImageSendMessage(original_content_url=imgurl,preview_image_url=imgurl)
-    elif '夜市' in msg:
-        imgurl='https://www.healingdaily.com.tw/media/images/2020/11/125507161_3685291601521188_119112334151225852_o_0_1606183965.jpg'#夜市
-        message = ImageSendMessage(original_content_url=imgurl,preview_image_url=imgurl)
-    elif '壽司' in msg:
-        foodout=[]
-        food=['https://img.ltn.com.tw/Upload/health/page/800/2021/08/24/phpH4bbQ1.jpg',#建議
-            'https://img.nutruelifegood.com/2021/03/1615964325-303941d08cfb8e7d20fd4b9f3c9159f8.jpg',#爭鮮
-             'https://pic.pimg.tw/drremin/1622165317-1690185448-g_l.jpg',#壽司郎
-             ]
-        for i in food:
-            foodout.append(ImageSendMessage(original_content_url=i,preview_image_url=i))
-        line_bot_api.reply_message(event.reply_token,foodout)
-    elif '火鍋' in msg:
-        foodout=[]
-        food=['https://images.chinatimes.com/newsphoto/2021-02-21/656/20210221003719.jpg',#全圖
-            'https://www.healingdaily.com.tw/media/images/2020/11/116935185_3393507374032947_1204503926942113906_o_1604920981.jpg',#湯底
-             'https://www.healingdaily.com.tw/media/images/2020/11/117367709_3393507187366299_7475865154440236430_o_1604920963.jpg',#火鍋料
-             'https://pic.pimg.tw/drremin/1624584009-808633920-g_l.png',#肉
-             'https://www.healingdaily.com.tw/media/images/2020/11/117326914_3393507254032959_4393351299018645612_o_1605001202.jpg',#醬料
-             ]
-        for i in food:
-            foodout.append(ImageSendMessage(original_content_url=i,preview_image_url=i))
-        line_bot_api.reply_message(event.reply_token,foodout)
-        
-    elif '早餐' in msg:
-        foodout=[]
-        food=['https://cdn2.ettoday.net/images/5133/5133040.jpg',#早餐吐司
-             'https://news.cts.com.tw/photo/cts/202011/202011262021924_l.jpg',#果醬吐司
-             'https://tw.maminews.com/img/articles/2020/10/500x320_1602125897ae.jpg',#早餐漢堡
-             'https://www.kingnet.com.tw/uploadFile/202101/1611740293-1317296updateNew1.jpeg',#早餐蛋餅
-             'https://www.kingnet.com.tw/uploadFile/202101/1611740642-1307644updateNew2.jpeg',#早餐點心
-             ]
-             #'https://img.ltn.com.tw/Upload/health/page/800/2021/03/29/phpcVDoDl.jpg'#中式早餐
-             
-        for i in food:
-            foodout.append(ImageSendMessage(original_content_url=i,preview_image_url=i))
-        line_bot_api.reply_message(event.reply_token,foodout)
-        #line_bot_api.reply_message(event.reply_token,[ImageSendMessage(original_content_url='https://cdn2.ettoday.net/images/5133/5133040.jpg',preview_image_url='https://cdn2.ettoday.net/images/5133/5133040.jpg'),
-        #                                              ImageSendMessage(original_content_url='https://news.cts.com.tw/photo/cts/202011/202011262021924_l.jpg',preview_image_url='https://news.cts.com.tw/photo/cts/202011/202011262021924_l.jpg')])
-        
-    elif '中式' in msg:
-        foodout=[]
-        food=['https://img.ltn.com.tw/Upload/health/page/800/2021/03/29/phpcVDoDl.jpg',#中式早餐
-             'https://cdn2.ettoday.net/images/5797/5797653.jpg',#麵食
-             'https://cdn2.ettoday.net/images/5567/5567264.jpg',#國民美食
-             'https://attach.setn.com/newsimages/2021/10/19/3368620-PH.jpg',#麵
-             ]
-
-             
-        for i in food:
-            foodout.append(ImageSendMessage(original_content_url=i,preview_image_url=i))
-        line_bot_api.reply_message(event.reply_token,foodout)
-    
-    elif 'subway' in msg:
-        foodout=[]
-        food=['https://resource01-proxy.ulifestyle.com.hk/res/v3/image/content/2575000/2578744/bbbb_600.png',
-             'https://i.pinimg.com/736x/24/1e/62/241e625de39b2d471f3cd6e8faf7892d.jpg']#醬
-        for i in food:
-            foodout.append(ImageSendMessage(original_content_url=i,preview_image_url=i))
-        line_bot_api.reply_message(event.reply_token,foodout)
-    
-    elif '吃什麼' in msg:
-         food=['https://cc.tvbs.com.tw/img/program/upload/2020/09/09/20200909204016-7fb822c3.jpg',
-                   'https://storage.googleapis.com/www-cw-com-tw/article/202101/article-5ff76e12dff12.jpg',
-                   'https://tw.savorjapan.com/gg/content_image/t0268_001.jpg',
-                   'https://www.citylink.tw/songshan/wp-content/uploads/M720.jpg',
-                   'https://foodtracer.taipei.gov.tw/Backend/upload//company/23309178/05da95d3-8373-4e74-b0c0-546977044f87.jpg',
-                   'https://findlife.com.tw/menu/blog/wp-content/uploads/2020/06/4.jpg',
-                   'https://snoopyblog.com/wp-content/uploads/2017/10/1509377834-3c81354d34ea77879a0733f3dfd63481.jpg']
-         x=random.randint(0,6)
-         imgurl=food[x]
-         message = ImageSendMessage(original_content_url=imgurl,preview_image_url=imgurl)
-    elif '常威' in msg :
-        msg='我天生神力'
-        message = TextSendMessage(text=msg)
-    elif '大樂透' in msg:
+  
+    elif '大樂透' == msg:
         x=random.sample(range(1,49),6)
         y=[]
         for i in x:
              y.append(str(i))
         msg=','.join(y)
         message = TextSendMessage(text=msg)
-    elif '新聞' in msg:
-        new=[]
-        for i in news():
-            new.append(TextSendMessage(text=i))
-        line_bot_api.reply_message(event.reply_token,new)
+
 
   #  else:
   #      if status == 2:
